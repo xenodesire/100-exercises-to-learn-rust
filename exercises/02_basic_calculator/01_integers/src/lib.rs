d975924100287364089ee60166f5a7833eb11496
BrunoCiccarino <landneco0@gmail.com>
2025-01-31 21:26:59 -0300

    feat: adding new solutions

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,7 +1,7 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
-    let multiplier: u8 = 4;
-    a + b * multiplier
+    let multiplier: u32 = 4;
+    return a + b * multiplier;
 }
 
 #[cfg(test)]
