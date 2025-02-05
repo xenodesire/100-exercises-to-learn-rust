6ddf93df21722acd216ea51fa38e49e352ea187b
BrunoCiccarino <landneco0@gmail.com>
2025-02-05 09:13:25 -0300

    feat: resolution of loop exercices

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,14 @@
 // Rewrite the factorial function using a `for` loop.
-pub fn factorial(n: u32) -> u32 {
-    todo!()
+pub fn factorial(mut n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        return 1;
+    }
+    let mut result: u32 = 1;
+    for i in 1..n {
+        result *= n;
+        n -= 1;
+    }
+    return result;
 }
 
 #[cfg(test)]
